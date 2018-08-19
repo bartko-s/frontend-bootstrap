43e366c8dab35e5ef1c6ace4555fa727574319e4
Bartko Stefan <bartko.stefan@gmail.com>
2018-08-19 17:02:35 +0200

    update

--- src/js/StatefullComponent.tsx
+++ src/js/StatefullComponent.tsx
@@ -1,11 +1,6 @@
 import * as React from 'react';
 import {ReactNode} from 'react';
 import {Button} from "./StatelessComponent"
-import {withDefaultProps} from "./helper"
-
-const initialState = {
-    icoClass: "ico",
-}
 
 type State = Readonly<typeof initialState>
 
@@ -13,53 +8,56 @@ type Props = {
     speed?: number,
 } & Readonly<typeof defaultProps>
 
+const initialState = {
+    icoClass: "ico",
+}
+
 const defaultProps = {
     speed: 1000
 }
 
-export const MyReactComponent = withDefaultProps(
-    defaultProps,
-    class MyReactComponent extends React.Component<Props, State> {
-        readonly state: State = initialState;
+export class MyReactComponent extends React.Component<Props, State> {
+    readonly state: State = initialState;
 
-        private intervalId?: number;
+    static defaultProps = defaultProps;
 
-        onButtonClick = () => {
+    private intervalId?: number;
+
+    onButtonClick = () => {
+        this.setState(reverseIco)
+    };
+
+    componentDidMount(): void {
+        this.startTimer()
+    }
+
+    componentWillUnmount(): void {
+        this.stopTimer()
+    }
+
+    startTimer = () => {
+        this.intervalId = window.setInterval(() => {
             this.setState(reverseIco)
-        };
+        }, this.props.speed)
+    }
 
-        componentDidMount(): void {
-            this.startTimer()
-        }
-
-        componentWillUnmount(): void {
-            this.stopTimer()
-        }
-
-        startTimer = () => {
-            this.intervalId = window.setInterval(() => {
-                this.setState(reverseIco)
-            }, this.props.speed)
-        }
-
-        stopTimer = () => {
-            if (this.intervalId !== undefined) {
-                clearInterval(this.intervalId);
-            }
-        }
-
-        render(): ReactNode {
-            return (
-                <div>
-                    <div className={this.state.icoClass}/>
-                    <Button onClick={this.onButtonClick}>Reverse</Button>
-                    <Button onClick={this.stopTimer}>Stop</Button>
-                    <Button onClick={this.startTimer}>Start</Button>
-                </div>
-            )
+    stopTimer = () => {
+        if (this.intervalId !== undefined) {
+            clearInterval(this.intervalId);
         }
     }
-)
+
+    render(): ReactNode {
+        return (
+            <div>
+                <div className={this.state.icoClass}/>
+                <Button onClick={this.onButtonClick}>Reverse</Button>
+                <Button onClick={this.stopTimer}>Stop</Button>
+                <Button onClick={this.startTimer}>Start</Button>
+            </div>
+        )
+    }
+}
 
 const reverseIco = (prevState: State) => ({
     icoClass: prevState.icoClass === "ico" ? "ico ico__reverse" : "ico",
