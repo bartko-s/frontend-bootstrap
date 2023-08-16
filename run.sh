5fe704143c52438b745b42b292cb9c08786418d7
Bartko Stefan <bartko.stefan@gmail.com>
2023-08-16 21:30:44 +0200

    CHANGE - run.sh script

--- run.sh
+++ run.sh
@@ -1,33 +1,68 @@
 #!/usr/bin/env bash
 
-PS3='Please select action: '
-options=("development" "production" "production build" "connect to browser container" "connect to server container" "quit")
-select opt in "${options[@]}"
-do
-    case $opt in
-        "development")
+RED='\033[0;31m'
+GREEN="\033[0;32m"
+YELLOW="\033[0;33m"
+BLUE="\033[0;34m"
+NC='\033[0m' # No Color
+
+function showHelp {
+    echo -e "
+    OPTIONS
+     - ${GREEN}development${NC} or ${YELLOW}dev${NC} - Start development server
+     - ${GREEN}production${NC} or ${YELLOW}prod${NC} - Start production server
+     - ${GREEN}production-build${NC} or ${YELLOW}build${NC} - Build
+     - ${GREEN}connect-to-browser-container${NC} or ${YELLOW}cb${NC} - Connect to browser container
+     - ${GREEN}connect-to-server-container${NC} or ${YELLOW}cs${NC} - Connect to server container
+     - ${GREEN}quit${NC} or ${YELLOW}q${NC} - Quit
+  "
+}
+
+commandName=
+userInput=$1
+showHelp=$userInput
+while [[ -z "$commandName" ]]; do
+    if [[ -z "$showHelp" ]]; then
+        showHelp
+    fi
+
+    if [[ -z "$userInput" ]]; then
+        read -r commandName
+        showHelp=
+    else
+        commandName="$userInput"
+        userInput=
+    fi
+
+    case ${commandName} in
+        "development" | "dev")
             docker-compose -f ./docker-compose-production.yml -f ./docker-compose-development.yml up --build
             break
             ;;
-        "production")
+        "production" | "prod")
             docker-compose -f ./docker-compose-production.yml up --build
             break
             ;;
-        "production build")
+        "production build" | "build")
             docker-compose -f ./docker-compose-production.yml -f ./docker-compose-build.yml up --build
             break
             ;;
-        "connect to browser container")
+        "connect to browser container" | "cb")
             docker-compose -f ./docker-compose-production.yml exec node-browser bash
             break
             ;;
-        "connect to server container")
+        "connect to server container" | "cs")
             docker-compose -f ./docker-compose-production.yml exec node-server bash
             break
             ;;
-        "quit")
+        "quit" | "q")
             break
             ;;
-        *) echo "invalid action $REPLY";;
-    esac
+        *)
+            showHelp
+            echo "Sorry. Unknown command \"$commandName\". Try again."
+            commandName=
+            showHelp='no'
+            ;;
+      esac
 done
